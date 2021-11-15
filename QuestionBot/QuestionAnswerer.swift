e74e0ef969a8f652d13e762ab836c2b61340f8c2
Matt Hanlon <mhanlon@mac.com>
2021-11-15 10:39:01 +0000

    Our checkpoint after the workshop on November 15th.

---
+++ QuestionBot/QuestionAnswerer.swift
@@ -0,0 +1,26 @@
+struct MyQuestionAnswerer {
+    func responseTo(question: String) -> String {
+        
+        let lowerQuestion = question.lowercased()
+        
+        if lowerQuestion == "where are the cookies?" {
+            return "In the cookie jar!"
+        } else if lowerQuestion.hasPrefix("where") {
+            return "To the North!"
+        } else if lowerQuestion.hasPrefix("hello") {
+            return "Why hello there!"
+        } else {
+          
+            let defaultNumber = question.count % 3
+            
+            if defaultNumber == 0 {
+                return "That really depends"
+            } else if defaultNumber == 1 {
+                return "No more cookies!"
+            } else {
+                return "Ask me again tomorrow"
+            }
+
+        }
+    }
+}
